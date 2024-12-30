eebd1d359df32825380467a918a98b0876b9f640
Helber Belmiro <helber.belmiro@gmail.com>
2024-12-30 11:17:53 -0300

    Reading historical features

--- feature_repo/example_repo.py
+++ feature_repo/example_repo.py
@@ -3,7 +3,6 @@
 from datetime import timedelta
 
 import pandas as pd
-
 from feast import (
     Entity,
     FeatureService,
@@ -16,45 +15,57 @@ from feast import (
 )
 from feast.feature_logging import LoggingConfig
 from feast.infra.offline_stores.file_source import FileLoggingDestination
-from feast.on_demand_feature_view import on_demand_feature_view
-from feast.types import Float32, Float64, Int64
+from feast.types import Float32, Int64, Bool
 
 # Define a project for the feature repo
 project = Project(name="fraud_detection_e2e_demo", description="A project for driver statistics")
 
 # Define an entity for the driver. You can think of an entity as a primary key used to
 # fetch features.
-driver = Entity(name="driver", join_keys=["driver_id"])
+transaction = Entity(name="transaction", join_keys=["transaction_id"])
+
+df = pd.read_csv("data/synthetic_train.csv")
+
+df["event_timestamp"] = pd.to_datetime(df["event_timestamp"], errors="coerce", utc=True)
+df["created_timestamp"] = pd.to_datetime(df["created_timestamp"], errors="coerce", utc=True)
+
+df.to_parquet('data/train.parquet')
 
 # Read data from parquet files. Parquet is convenient for local development mode. For
 # production, you can use your favorite DWH, such as BigQuery. See Feast documentation
 # for more info.
-driver_stats_source = FileSource(
-    name="driver_hourly_stats_source",
-    path="data/driver_stats.parquet",
+transaction_source = FileSource(
+    name="transaction_stats_source",
+    path="data/train.parquet",
     timestamp_field="event_timestamp",
-    created_timestamp_column="created",
+    created_timestamp_column="created_timestamp",
 )
 
 # Our parquet files contain sample data that includes a driver_id column, timestamps and
 # three feature column. Here we define a Feature View that will allow us to serve this
 # data to our model online.
-driver_stats_fv = FeatureView(
+transactions_fv = FeatureView(
     # The unique name of this feature view. Two feature views in a single
     # project cannot have the same name
-    name="driver_hourly_stats",
-    entities=[driver],
+    name="transactions",
+    entities=[transaction],
     ttl=timedelta(days=1),
     # The list of features defined below act as a schema to both define features
     # for both materialization of features into a store, and are used as references
     # during retrieval for building a training dataset or serving features
     schema=[
-        Field(name="conv_rate", dtype=Float32),
-        Field(name="acc_rate", dtype=Float32),
-        Field(name="avg_daily_trips", dtype=Int64, description="Average daily trips"),
+        Field(name="distance_from_home", dtype=Float32),
+        Field(name="distance_from_last_transaction", dtype=Float32),
+        Field(name="ratio_to_median_purchase_price", dtype=Float32),
+        Field(name="repeat_retailer", dtype=Bool),
+        Field(name="used_chip", dtype=Bool),
+        Field(name="used_pin_number", dtype=Bool),
+        Field(name="online_order", dtype=Bool),
+        Field(name="fraud", dtype=Bool),
+
     ],
     online=True,
-    source=driver_stats_source,
+    source=transaction_source,
     # Tags are user defined key/value pairs that are attached to each
     # feature view
     tags={"team": "driver_performance"},
@@ -70,79 +81,31 @@ input_request = RequestSource(
     ],
 )
 
-
-# Define an on demand feature view which can generate new features based on
-# existing feature views and RequestSource features
-@on_demand_feature_view(
-    sources=[driver_stats_fv, input_request],
-    schema=[
-        Field(name="conv_rate_plus_val1", dtype=Float64),
-        Field(name="conv_rate_plus_val2", dtype=Float64),
-    ],
-)
-def transformed_conv_rate(inputs: pd.DataFrame) -> pd.DataFrame:
-    df = pd.DataFrame()
-    df["conv_rate_plus_val1"] = inputs["conv_rate"] + inputs["val_to_add"]
-    df["conv_rate_plus_val2"] = inputs["conv_rate"] + inputs["val_to_add_2"]
-    return df
-
-
 # This groups features into a model version
-driver_activity_v1 = FeatureService(
-    name="driver_activity_v1",
+transactions_fs = FeatureService(
+    name="transactions_fs",
     features=[
-        driver_stats_fv[["conv_rate"]],  # Sub-selects a feature from a feature view
-        transformed_conv_rate,  # Selects all features from the feature view
+        transactions_fv
     ],
     logging_config=LoggingConfig(
         destination=FileLoggingDestination(path="data")
     ),
 )
-driver_activity_v2 = FeatureService(
-    name="driver_activity_v2", features=[driver_stats_fv, transformed_conv_rate]
-)
 
 # Defines a way to push data (to be available offline, online or both) into Feast.
-driver_stats_push_source = PushSource(
-    name="driver_stats_push_source",
-    batch_source=driver_stats_source,
+transactions_push_source = PushSource(
+    name="transactions_push_source",
+    batch_source=transaction_source,
 )
 
 # Defines a slightly modified version of the feature view from above, where the source
 # has been changed to the push source. This allows fresh features to be directly pushed
 # to the online store for this feature view.
-driver_stats_fresh_fv = FeatureView(
-    name="driver_hourly_stats_fresh",
-    entities=[driver],
+transactions_fresh_fv = FeatureView(
+    name="transactions_fresh",
+    entities=[transaction],
     ttl=timedelta(days=1),
-    schema=[
-        Field(name="conv_rate", dtype=Float32),
-        Field(name="acc_rate", dtype=Float32),
-        Field(name="avg_daily_trips", dtype=Int64),
-    ],
     online=True,
-    source=driver_stats_push_source,  # Changed from above
+    source=transactions_push_source,  # Changed from above
     tags={"team": "driver_performance"},
-)
-
-
-# Define an on demand feature view which can generate new features based on
-# existing feature views and RequestSource features
-@on_demand_feature_view(
-    sources=[driver_stats_fresh_fv, input_request],  # relies on fresh version of FV
-    schema=[
-        Field(name="conv_rate_plus_val1", dtype=Float64),
-        Field(name="conv_rate_plus_val2", dtype=Float64),
-    ],
-)
-def transformed_conv_rate_fresh(inputs: pd.DataFrame) -> pd.DataFrame:
-    df = pd.DataFrame()
-    df["conv_rate_plus_val1"] = inputs["conv_rate"] + inputs["val_to_add"]
-    df["conv_rate_plus_val2"] = inputs["conv_rate"] + inputs["val_to_add_2"]
-    return df
-
-
-driver_activity_v3 = FeatureService(
-    name="driver_activity_v3",
-    features=[driver_stats_fresh_fv, transformed_conv_rate_fresh],
 )
--- feature_repo/test_workflow.py
+++ feature_repo/test_workflow.py
@@ -2,59 +2,57 @@ import subprocess
 from datetime import datetime
 
 import pandas as pd
-
 from feast import FeatureStore
-from feast.data_source import PushMode
 
 
 def run_demo():
     store = FeatureStore(repo_path=".")
     print("\n--- Run feast apply ---")
-    subprocess.run(["feast", "apply"])
+    # subprocess.run(["feast", "apply"])
 
     print("\n--- Historical features for training ---")
     fetch_historical_features_entity_df(store, for_batch_scoring=False)
 
-    print("\n--- Historical features for batch scoring ---")
-    fetch_historical_features_entity_df(store, for_batch_scoring=True)
-
-    print("\n--- Load features into online store ---")
-    store.materialize_incremental(end_date=datetime.now())
-
-    print("\n--- Online features ---")
-    fetch_online_features(store)
-
-    print("\n--- Online features retrieved (instead) through a feature service---")
-    fetch_online_features(store, source="feature_service")
-
-    print(
-        "\n--- Online features retrieved (using feature service v3, which uses a feature view with a push source---"
-    )
-    fetch_online_features(store, source="push")
-
-    print("\n--- Simulate a stream event ingestion of the hourly stats df ---")
-    event_df = pd.DataFrame.from_dict(
-        {
-            "driver_id": [1001],
-            "event_timestamp": [
-                datetime.now(),
-            ],
-            "created": [
-                datetime.now(),
-            ],
-            "conv_rate": [1.0],
-            "acc_rate": [1.0],
-            "avg_daily_trips": [1000],
-        }
-    )
-    print(event_df)
-    store.push("driver_stats_push_source", event_df, to=PushMode.ONLINE_AND_OFFLINE)
-
-    print("\n--- Online features again with updated values from a stream push---")
-    fetch_online_features(store, source="push")
-
-    print("\n--- Run feast teardown ---")
-    subprocess.run(["feast", "teardown"])
+    # print("\n--- Historical features for batch scoring ---")
+    # fetch_historical_features_entity_df(store, for_batch_scoring=True)
+    #
+    # print("\n--- Load features into online store ---")
+    # store.materialize_incremental(end_date=datetime.now())
+    #
+    # print("\n--- Online features ---")
+    # fetch_online_features(store)
+    #
+    # print("\n--- Online features retrieved (instead) through a feature service---")
+    # fetch_online_features(store, source="feature_service")
+    #
+    # print(
+    #     "\n--- Online features retrieved (using feature service v3, which uses a feature view with a push source---"
+    # )
+    # fetch_online_features(store, source="push")
+    #
+    # print("\n--- Simulate a stream event ingestion of the hourly stats df ---")
+    # event_df = pd.DataFrame.from_dict(
+    #     {
+    #         "driver_id": [1001],
+    #         "event_timestamp": [
+    #             datetime.now(),
+    #         ],
+    #         "created": [
+    #             datetime.now(),
+    #         ],
+    #         "conv_rate": [1.0],
+    #         "acc_rate": [1.0],
+    #         "avg_daily_trips": [1000],
+    #     }
+    # )
+    # print(event_df)
+    # store.push("driver_stats_push_source", event_df, to=PushMode.ONLINE_AND_OFFLINE)
+    #
+    # print("\n--- Online features again with updated values from a stream push---")
+    # fetch_online_features(store, source="push")
+    #
+    # print("\n--- Run feast teardown ---")
+    # subprocess.run(["feast", "teardown"])
 
 
 def fetch_historical_features_entity_df(store: FeatureStore, for_batch_scoring: bool):
@@ -63,20 +61,16 @@ def fetch_historical_features_entity_df(store: FeatureStore, for_batch_scoring: 
     entity_df = pd.DataFrame.from_dict(
         {
             # entity's join key -> entity values
-            "driver_id": [1001, 1002, 1003],
+            "transaction_id": [1, 2, 3],
             # "event_timestamp" (reserved key) -> timestamps
             "event_timestamp": [
-                datetime(2021, 4, 12, 10, 59, 42),
-                datetime(2021, 4, 12, 8, 12, 10),
-                datetime(2021, 4, 12, 16, 40, 26),
+                datetime(2024, 12, 1, 4, 17, 47, 688222),
+                datetime(2024, 12, 5, 7, 22, 19, 688222),
+                datetime(2024, 12, 10, 11, 27, 18, 688222),
             ],
-            # (optional) label name -> label values. Feast does not process these
-            "label_driver_reported_satisfaction": [1, 5, 3],
-            # values we're using for an on-demand transformation
-            "val_to_add": [1, 2, 3],
-            "val_to_add_2": [10, 20, 30],
         }
     )
+
     # For batch scoring, we want the latest timestamps
     if for_batch_scoring:
         entity_df["event_timestamp"] = pd.to_datetime("now", utc=True)
@@ -84,11 +78,9 @@ def fetch_historical_features_entity_df(store: FeatureStore, for_batch_scoring: 
     training_df = store.get_historical_features(
         entity_df=entity_df,
         features=[
-            "driver_hourly_stats:conv_rate",
-            "driver_hourly_stats:acc_rate",
-            "driver_hourly_stats:avg_daily_trips",
-            "transformed_conv_rate:conv_rate_plus_val1",
-            "transformed_conv_rate:conv_rate_plus_val2",
+            "transactions:distance_from_home",
+            "transactions:distance_from_last_transaction",
+            "transactions:fraud",
         ],
     ).to_df()
     print(training_df.head())
