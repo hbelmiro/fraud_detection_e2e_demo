0f7f83ed05a0b18b0e00bba40468f64829cad197
Helber Belmiro <helber.belmiro@gmail.com>
2025-01-21 10:39:08 -0300

    Feature being applied

--- feature_engineering/feature_repo/example_repo.py
+++ feature_engineering/feature_repo/example_repo.py
@@ -1,7 +1,8 @@
 # This is an example feature definition file
-
+import os
 from datetime import timedelta
 
+import feast.types
 import pandas as pd
 from feast import (
     Entity,
@@ -15,32 +16,35 @@ from feast import (
 )
 from feast.feature_logging import LoggingConfig
 from feast.infra.offline_stores.file_source import FileLoggingDestination
-from feast.types import Float32, Int64, Bool
+
+from feature_engineering import get_features, data_dir
+
+
+features = get_features()
+features_file_name = os.path.join(data_dir, "features.csv")
+features.to_csv(features_file_name)
 
 # Define a project for the feature repo
 project = Project(name="fraud_detection_e2e_demo", description="A project for driver statistics")
-
 # Define an entity for the driver. You can think of an entity as a primary key used to
 # fetch features.
 transaction = Entity(name="transaction", join_keys=["user_id"])
-
-df = pd.read_csv("data/final_data.csv")
-
+df = pd.read_csv(features_file_name)
 df["created"] = pd.to_datetime(df["created"], errors="coerce", utc=True)
 df["updated"] = pd.to_datetime(df["updated"], errors="coerce", utc=True)
 
-df.to_parquet('data/final_data.parquet')
+parquet_file_name = features_file_name.replace(".csv", ".parquet")
 
+df.to_parquet(parquet_file_name)
 # Read data from parquet files. Parquet is convenient for local development mode. For
 # production, you can use your favorite DWH, such as BigQuery. See Feast documentation
 # for more info.
 transaction_source = FileSource(
     name="transaction_stats_source",
-    path="data/final_data.parquet",
+    path=parquet_file_name,
     timestamp_field="created",
     created_timestamp_column="updated",
 )
-
 # Our parquet files contain sample data that includes a driver_id column, timestamps and
 # three feature column. Here we define a Feature View that will allow us to serve this
 # data to our model online.
@@ -54,15 +58,23 @@ transactions_fv = FeatureView(
     # for both materialization of features into a store, and are used as references
     # during retrieval for building a training dataset or serving features
     schema=[
-        Field(name="distance_from_home", dtype=Float32),
-        Field(name="distance_from_last_transaction", dtype=Float32),
-        Field(name="ratio_to_median_purchase_price", dtype=Float32),
-        Field(name="repeat_retailer", dtype=Bool),
-        Field(name="used_chip", dtype=Bool),
-        Field(name="used_pin_number", dtype=Bool),
-        Field(name="online_order", dtype=Bool),
-        Field(name="fraud", dtype=Bool),
-
+        Field(name="user_id", dtype=feast.types.String),
+        Field(name="fraud", dtype=feast.types.Float32),
+        Field(name="created", dtype=feast.types.String),
+        Field(name="updated", dtype=feast.types.String),
+        Field(name="set", dtype=feast.types.String),
+        Field(name="distance_from_home", dtype=feast.types.Float32),
+        Field(name="distance_from_last_transaction", dtype=feast.types.Float32),
+        Field(name="ratio_to_median_purchase_price", dtype=feast.types.Float32),
+        Field(name="num_prev_transactions", dtype=feast.types.Float32),
+        Field(name="avg_prev_transaction_amount", dtype=feast.types.Float32),
+        Field(name="max_prev_transaction_amount", dtype=feast.types.Float32),
+        Field(name="stdv_prev_transaction_amount", dtype=feast.types.Float32),
+        Field(name="days_since_last_transaction", dtype=feast.types.Float32),
+        Field(name="days_since_first_transaction", dtype=feast.types.Float32),
+        Field(name="used_chip", dtype=feast.types.Bool),
+        Field(name="used_pin_number", dtype=feast.types.Bool),
+        Field(name="online_order", dtype=feast.types.Bool),
     ],
     online=True,
     source=transaction_source,
@@ -70,17 +82,15 @@ transactions_fv = FeatureView(
     # feature view
     tags={"team": "driver_performance"},
 )
-
 # Define a request data source which encodes features / information only
 # available at request time (e.g. part of the user initiated HTTP request)
 input_request = RequestSource(
     name="vals_to_add",
     schema=[
-        Field(name="val_to_add", dtype=Int64),
-        Field(name="val_to_add_2", dtype=Int64),
+        Field(name="val_to_add", dtype=feast.types.Int64),
+        Field(name="val_to_add_2", dtype=feast.types.Int64),
     ],
 )
-
 # This groups features into a model version
 transactions_fs = FeatureService(
     name="transactions_fs",
@@ -91,13 +101,11 @@ transactions_fs = FeatureService(
         destination=FileLoggingDestination(path="data")
     ),
 )
-
 # Defines a way to push data (to be available offline, online or both) into Feast.
 transactions_push_source = PushSource(
     name="transactions_push_source",
     batch_source=transaction_source,
 )
-
 # Defines a slightly modified version of the feature view from above, where the source
 # has been changed to the push source. This allows fresh features to be directly pushed
 # to the online store for this feature view.
@@ -109,3 +117,4 @@ transactions_fresh_fv = FeatureView(
     source=transactions_push_source,  # Changed from above
     tags={"team": "driver_performance"},
 )
+
