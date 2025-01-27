bd7cd1d14a9ee447691dc53d34023a403caa4b11
Helber Belmiro <helber.belmiro@gmail.com>
2025-01-27 11:11:20 -0300

    Added Spark

--- train/test_model.py
+++ train/test_model.py
@@ -1,29 +1,40 @@
-import pickle
-
 import numpy as np
 import onnxruntime as rt
+from pyspark.ml.feature import StandardScalerModel
+from pyspark.ml.linalg import Vectors
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("FraudDetection").getOrCreate()
 
 # Load the test data and scaler:
+scaler_model = StandardScalerModel.load("artifact/scaler_model")
+test_df = spark.read.parquet("artifact/test_data.parquet")
 
-with open('artifact/scaler.pkl', 'rb') as handle:
-    scaler = pickle.load(handle)
-with open('artifact/test_data.pkl', 'rb') as handle:
-    (X_test, y_test) = pickle.load(handle)
+# Split the features and labels
+X_test = np.array([row.features.toArray() for row in test_df.collect()])
+y_test = np.array([row.label for row in test_df.collect()])
 
-# Create an ONNX inference runtime session and predict values for all test inputs:
+# Use the scaler model to transform the test data
+scaled_test_df = scaler_model.transform(test_df)
 
+# Extract scaled features and convert them to a NumPy array
+X_test_scaled = np.array([row.scaled_features.toArray() for row in scaled_test_df.collect()])
+
+# Load the ONNX model for inference
 sess = rt.InferenceSession("models/fraud/1/model.onnx", providers=rt.get_available_providers())
 input_name = sess.get_inputs()[0].name
 output_name = sess.get_outputs()[0].name
-y_pred_temp = sess.run([output_name], {input_name: X_test.astype(np.float32)})
+
+# Make predictions
+y_pred_temp = sess.run([output_name], {input_name: X_test_scaled.astype(np.float32)})
 y_pred_temp = np.asarray(np.squeeze(y_pred_temp[0]))
+
+# Apply threshold to predictions
 threshold = 0.95
 y_pred = np.where(y_pred_temp > threshold, 1, 0)
 
 # Show the results:
-
 from sklearn.metrics import precision_score, recall_score, confusion_matrix, ConfusionMatrixDisplay
-import numpy as np
 
 y_test_arr = y_test.squeeze()
 correct = np.equal(y_pred, y_test_arr).sum().item()
@@ -53,9 +64,15 @@ sally_transaction_details = [
     1.0,
     0.0,
     0.0]
-    ]
+]
 
-prediction = sess.run([output_name], {input_name: scaler.transform(sally_transaction_details).astype(np.float32)})
+sally_df = spark.createDataFrame([(Vectors.dense(sally_transaction_details[0]),)], ["features"])
+
+scaled_sally_df = scaler_model.transform(sally_df)
+
+scaled_sally = np.array([row.scaled_features.toArray() for row in scaled_sally_df.collect()])
+
+prediction = sess.run([output_name], {input_name: scaled_sally.astype(np.float32)})
 
 print("Is Sally's transaction predicted to be fraudulent? (true = YES, false = NO) ")
 print(np.squeeze(prediction) > threshold)
