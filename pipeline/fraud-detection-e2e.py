595d12b3ebf409cc345ebd5bc36ba32c0f1ec08f
Helber Belmiro <helber.belmiro@gmail.com>
2026-02-12 14:32:15 -0300

    force failure
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- pipeline/fraud-detection-e2e.py
+++ pipeline/fraud-detection-e2e.py
@@ -76,6 +76,8 @@ def prepare_data(job_id: str, data_preparation_image: str) -> bool:
     start_time = datetime.now()
     timeout_time = start_time + timedelta(minutes=timeout_minutes)
 
+    print("test")
+
     while True:
         if datetime.now() > timeout_time:
             logger.error(f"Timeout reached after {timeout_minutes} minutes waiting for Spark job completion")
